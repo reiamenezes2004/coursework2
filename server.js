ad02b4d797cdf0d539924477ad00f78e170cd546
Reia Menezes <RM1661@live.mdx.ac.uk>
2024-02-12 02:19:18 +0400

    pages attempt

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 // CORS middleware
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://reiamenezes2004.github.io/coursework2/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
