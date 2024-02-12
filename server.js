60e9a208ab9dc48c3cba659f60f03c085db59de4
Reia Menezes <RM1661@live.mdx.ac.uk>
2024-02-12 19:26:18 +0400

    search

--- server.js
+++ server.js
@@ -3,16 +3,9 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const { ObjectId } = require('mongodb');
 const path = require('path');
-const cors = require('cors');
 
-// Middleware
-app.use(express.json());
+// var apiRouter = require("./routes/api_router");
 
-// Define the directory where lesson images are stored
-const imagesDir = path.resolve(__dirname, 'images');
-
-// Middleware to serve static files (lesson images)
-app.use('/images', express.static(imagesDir));
 
 // Logger middleware function
 app.use((req, res, next) => {
@@ -20,13 +13,21 @@ app.use((req, res, next) => {
     next(); // Pass control to the next middleware function
 });
 
+// app.use(express.json());
+
 // CORS middleware
-const corsOptions = {
-    origin: 'https://reiamenezes2004.github.io', // Update with your frontend origin
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-};
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+// Serve lesson images from the 'coursework2/images' directory
+app.use('/coursework2/images', express.static('coursework2/images'));
+
+// Setting the port
+app.set('port', 4000);
 
 let db;
 
@@ -65,29 +66,40 @@ app.post('/collection/:collectionName', (req, res, next) => {
     });
 });
 
-app.post('/orders', (req, res) => {
-    const order = req.body;
-
-    if (!order || !order.cartItems || !Array.isArray(order.cartItems)) {
-        return res.status(400).json({ error: 'Invalid order data' });
-    }
-
-    const orderedLessons = order.cartItems.map(item => item.title);
-    order.lessonsOrdered = orderedLessons;
-
-    db.collection('orders').insertOne(order, (err, result) => {
-        if (err) {
-            console.error('Error inserting order into database:', err);
-            return res.status(500).json({ error: 'Error placing order' });
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.updateOne(
+        { _id: new ObjectId(req.params.id) },
+        { $set: req.body },
+        (err, result) => {
+            if (err) return next(err);
+            res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' });
         }
-        console.log('Order placed successfully');
-        return res.status(201).json({ message: 'Order placed successfully', orderId: result.insertedId });
-    });
+    );
 });
 
-app.put('/orders', (req, res) => {
-    console.log('PUT /orders endpoint reached'); // Add this logging statement
 
+// app.get('/search', async (req, res) => {
+//     const searchTerm = req.query.q;
+
+//     if (!searchTerm) {
+//         return res.status(400).json({ error: 'Please provide a search term' });
+//     }
+
+//     try {
+//         const searchResults = await db.collection('lessons').find({
+//             $or: [
+//                 { title: { $regex: searchTerm, $options: 'i' } },
+//                 { location: { $regex: searchTerm, $options: 'i' } }
+//             ]
+//         }).toArray();
+//         res.status(200).json({ results: searchResults });
+//     } catch (error) {
+//         console.error('Error searching for lessons:', error);
+//         res.status(500).json({ error: 'Internal server error' });
+//     }
+// });
+
+app.post('/orders', (req, res) => {
     const order = req.body;
 
     if (!order.cartItems || !Array.isArray(order.cartItems)) {
@@ -97,31 +109,85 @@ app.put('/orders', (req, res) => {
     const orderedLessons = order.cartItems.map(item => item.title);
     order.lessonsOrdered = orderedLessons;
 
-    // Log the order details
-    console.log('Received order:', order);
-
-    // Update the available inventory for each lesson in the order
-    order.cartItems.forEach(cartItem => {
-        const lessonId = cartItem.id;
-        const newAvailability = cartItem.availableInventory - 1; // Assuming available inventory is decremented by 1
-        console.log('Updating inventory for lesson:', lessonId, 'New availability:', newAvailability); // Add this logging statement
-        db.collection('lessons').updateOne(
-            { _id: ObjectId(lessonId) },
-            { $set: { availableInventory: newAvailability } },
-            (err, result) => {
-                if (err) {
-                    console.error('Error updating available inventory for lesson:', lessonId, err);
-                    // Handle error (e.g., rollback transaction)
-                }
-                console.log('Available inventory updated successfully for lesson:', lessonId); // Add this logging statement
-                // Handle success (e.g., send response to client)
-            }
-        );
+    db.collection('orders').insertOne(order, (err, result) => {
+        if (err) {
+            console.error('Error placing order:', err);
+            return res.status(500).json({ error: 'Error placing order' });
+        }
+        console.log('Order placed successfully');
+        return res.status(201).json({ message: 'Order placed successfully', orderId: result.insertedId });
     });
-
-    console.log('Order placed successfully');
-    return res.status(200).json({ message: 'Order placed successfully' });
 });
+
+app.put('/collection/lessons/:id', (req, res, next) => {
+    const updatedInventory = req.body.availableInventory;
+
+    db.collection('lessons').updateOne(
+        { _id: new ObjectId(req.params.id) },
+        { $inc: { availableInventory: -1 } },
+        (err, result) => {
+            if (err) {
+                console.error('Error updating inventory:', err);
+                res.status(500).json({ error: 'Error updating inventory' });
+                return;
+            }
+            if (result.modifiedCount === 1) {
+                console.log('Inventory updated successfully');
+                res.status(200).json({ msg: 'success' });
+            } else {
+                console.log('Failed to update inventory');
+                res.status(404).json({ error: 'Failed to update inventory' });
+            }
+        }
+    );
+});
+
+
+const searchLessons = async (searchTerm) => {
+    try {
+        const response = await fetch(`/search?q=${searchTerm}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const results = await response.json();
+        console.log(results); // Handle the search results (update UI, etc.)
+    } catch (error) {
+        console.error('Error searching for lessons:', error);
+    }
+};
+
+app.get('/search', async (req, res) => {
+    const searchTerm = req.query.q;
+
+    if (!searchTerm) {
+        return res.status(400).json({ error: 'Please provide a search term' });
+    }
+
+    try {
+        const searchResults = await db.collection('lessons').find({
+            $or: [
+                { title: { $regex: searchTerm, $options: 'i' } },
+                { location: { $regex: searchTerm, $options: 'i' } }
+            ]
+        }).toArray();
+        res.status(200).json({ results: searchResults });
+    } catch (error) {
+        console.error('Error searching for lessons:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+
+
+app.get('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.findOne({ _id: new ObjectId(req.params.id) }, (err, result) => {
+        if (err) return next(err);
+        res.send(result);
+    });
+});
+
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
